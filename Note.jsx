5379c863612b499dc9f6c85d4aff87d1b27a02d1
revathyp2001 <85251061+revathyp2001@users.noreply.github.com>
2021-06-05 21:36:50 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Basic Front End Web Dev with Javascript & React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha.Through this
+        bootcamp, we covered everything from Scratch including Javascript,
+        React.js and HTML.This was a 7 days free bootcamp on Basic Web Dev in
+        Collaboration with Google Developer Groups, Microsoft Learn Student
+        Ambassador Program and AWS Students Club..
+      </p>
+    </div>
+  );
+}
+
+export default Note;
